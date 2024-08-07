1b7d03cb108f649cab8b7b58e6b5f3da082ea4bf
Axilleas <achixoum@gmail.com>
2024-08-07 18:28:59 +0300

    git commit -m "Fix a bug"

--- main.cpp
+++ main.cpp
@@ -119,6 +119,15 @@ int main(int argc, char* argv[])
                 reset.Update(x,y);
             playAgain.Update(x,y);
             quit.Update(x,y);
+            if (j==9 && !flag)
+            {
+                playAgain.SetRectProperties((800-500)/2+150,(800-500)/2+150,200,100);
+                quit.SetRectProperties((800-500)/2+150,(800-500)/2+250,200,100);
+                text1.SetText("DRAW", 20, 20, 20);
+                text1.SetTexture(renderer);
+                text1.SetRectProperties((800-500)/2-50,(800-500)/2+50,500,100);
+                flag = true;
+            }
             if (event.type == SDL_MOUSEBUTTONDOWN && event.button.button == SDL_BUTTON_LEFT)
             {
                 board.setX(x);
@@ -155,17 +164,9 @@ int main(int argc, char* argv[])
                                     text1.SetRectProperties((800-500)/2-50,(800-500)/2+50,500,100);
                                     flag = true;
                                 }
-                                j++;
-                                if (j==9)
-                                {
-                                    playAgain.SetRectProperties((800-500)/2+150,(800-500)/2+150,200,100);
-                                    quit.SetRectProperties((800-500)/2+150,(800-500)/2+250,200,100);
-                                    text1.SetText("DRAW", 20, 20, 20);
-                                    text1.SetTexture(renderer);
-                                    text1.SetRectProperties((800-500)/2-50,(800-500)/2+50,500,100);
-                                    flag = true;
-                                }
                             }
+                            j++;
+                            SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION,"%d",j);
                         }
                     }
                 }
@@ -185,6 +186,7 @@ int main(int argc, char* argv[])
                     text1.SetTexture(renderer);
                     text1.SetRectProperties(0,650,500,100);
                     quit.SetRectProperties(-800,0,200,100);
+                    playAgain.SetRectProperties(-800,100,200,100);
                     flag = false;
                 }
                 if (quit.IsSelected()) {
